63d09cff8612a882b59391f408f677052feecaf1
ashikbillah699 <ashikbillah699@gmail.com>
2025-05-06 17:39:08 +0600

    Full CRUD functionality completed for Created Tasks (add, edit, delete)

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const app = express();
 const cors = require("cors");
@@ -14,52 +14,80 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    
-    const taskCollection = client.db("taskFlowDB").collection("tasks");
-    const userCollection = client.db("taskFlowDB").collection("users");
+    try {
 
-    // insert task
-    app.post('/task', async (req, res)=>{
-        const reciveData = req.body;
-        const result = await taskCollection.insertOne(reciveData);
-        res.send(result);
-    })
+        const taskCollection = client.db("taskFlowDB").collection("tasks");
+        const userCollection = client.db("taskFlowDB").collection("users");
 
-    // insert user
-    app.post('/user', async (req, res)=>{
-        const reciveData = req.body;
-        const result = await userCollection.insertOne(reciveData);
-        res.send(result);
-    })
+        // insert task
+        app.post('/task', async (req, res) => {
+            const reciveData = req.body;
+            const result = await taskCollection.insertOne(reciveData);
+            res.send(result);
+        })
 
-    // get All tasks
-    app.get('/tasks', async (req, res)=>{
-        const result = await taskCollection.find().toArray();
-        res.send(result)
-    })
+        // insert user
+        app.post('/user', async (req, res) => {
+            const reciveData = req.body;
+            const result = await userCollection.insertOne(reciveData);
+            res.send(result);
+        })
 
-    // get All users
-    app.get('/users', async (req, res)=>{
-        const result = await userCollection.find().toArray();
-        res.send(result)
-    })
+        // get All tasks
+        app.get('/tasks', async (req, res) => {
+            const result = await taskCollection.find().toArray();
+            res.send(result)
+        })
+
+        // get All users
+        app.get('/users', async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result)
+        })
+
+        // update task
+        app.put('/task/:id', async (req, res) => {
+            const id = req.params.id;
+            const updateTask = req.body;
+            const query = { _id: new ObjectId(id) }
+            const updateData = {
+                $set: {
+                    title: updateTask.title,
+                    description: updateTask.description,
+                    dueDate: updateTask.dueDate,
+                    priority: updateTask.priority,
+                    status: updateTask.status,
+                    assignedEmail: updateTask.assignedEmail,
+                    userEmail: updateTask.userEmail,
+                }
+            }
+            const result = await taskCollection.updateOne(query, updateData);
+            res.send(result)
+        })
+
+        // delete task
+        app.delete('/task/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await taskCollection.deleteOne(query);
+            res.send(result);
+        })
 
 
 
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
